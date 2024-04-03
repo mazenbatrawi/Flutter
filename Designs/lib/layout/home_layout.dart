362840df953a3b6ad213becf2a7a59511d0154b4
Mazen <1190102@student.birzeit.edu>
2024-04-03 03:09:08 +0300

    Creating SQFLite Database)

--- Designs/lib/layout/home_layout.dart
+++ Designs/lib/layout/home_layout.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:projects/modules/archived_tasks/archived_tasks_screen.dart';
 import 'package:projects/modules/done_tasks/done_tasks_screen.dart';
 import 'package:projects/modules/new_tasks/new_tasks_screen.dart';
+import 'package:sqflite/sqflite.dart';
 
 class HomeLayout extends StatefulWidget {
   const HomeLayout({super.key});
@@ -24,6 +25,12 @@ class _HomeLayoutState extends State<HomeLayout> {
     'Done Tasks',
     'Archived Tasks',
   ];
+
+  @override
+  void initState() {
+    super.initState();
+    createDatabase();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -82,4 +89,29 @@ class _HomeLayoutState extends State<HomeLayout> {
       body: screens[currentIndex],
     );
   }
+
+  void createDatabase() async {
+    var  database = await openDatabase(
+      'todo.db',
+      version: 1,
+      onCreate: (database, version) {
+        print('Database created');
+         database.execute(
+          'CREATE TABLE tasks (id INTEGER PRIMARY KEY, title TEXT, date TEXT, time TEXT, status TEXT)'
+        ).then((value) {
+          print('Table created');
+        }).catchError((error) {
+          print('Error while creating table ${error.toString()}');
+        });
+      },
+      onOpen: (database) {
+        print('Database opened');
+      },
+    );
+  }
+
+  void insertToDatabase() {
+
+  }
+
 }
